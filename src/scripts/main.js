871e75f372c5d958041325999e71707699d18df0
Csillag61 <csillagok@gmail.com>
2025-05-11 13:19:23 -0400

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const tree = document.querySelector('.tree');
+
+  if (!tree) {
+    // eslint-disable-next-line no-console
+    console.error('tree element not found');
+
+    return;
+  }
+
+  tree.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+      const childUl = e.target.querySelector('ul');
+
+      if (childUl) {
+        childUl.style.display =
+          childUl.style.display === 'none' ? 'block' : 'none';
+      }
+    }
+  });
+
+  tree.querySelectorAll('ul').forEach((ul) => (ul.style.display = 'none'));
+});
